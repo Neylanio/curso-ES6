cb482a93614b7fdad69292d24cd995fd50cfa6ad
Neylanio Soares <neylaniogomes@gmail.com>
2020-06-09 22:57:48 -0300

    To do Async/await challenge

---
+++ challenges/third.js
@@ -0,0 +1,71 @@
+
+//Utilize o async/await nos trechos de codigos
+
+// Funão delay aciona o .then após 1s
+// const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
+
+// async function umPorSegundo() {
+//     await delay();
+//     console.log('1s');
+//     await delay();
+//     console.log('2s');
+//     await delay();
+//     console.log('3s');
+// }
+
+// umPorSegundo();
+
+
+// ----------------------------------------------------------------------------
+
+
+// import axios from 'axios';
+
+// async function getUserFromGithub(user) {
+//     try{
+//         const response = await axios.get(`https://api.github.com/users/${user}`);
+//         console.log(response.data);
+//     }catch(error){
+//         console.log('Usuário não existe');
+//     }
+// }
+
+// getUserFromGithub('diego3g');
+// getUserFromGithub('diego3g124123');
+
+
+
+// -------------------------------------------------------------------------------
+
+// import axios from 'axios'
+
+// class Github {
+//     static async getRepositories(repo) {
+//         try{
+//             const response = await axios.get(`https://api.github.com/repos/${repo}`);
+//             console.log(response.data);
+//         }catch(error){
+//             console.log('Repositório não existe');
+//         }
+//     }
+// }
+
+// Github.getRepositories('Neylanio/curso-ES6');
+// Github.getRepositories('rocketseat/dslkvmskv');
+
+
+// -------------------------------------------------------------------------------
+
+
+// import axios from 'axios';
+
+// const buscaUsuario = async usuario => {
+//     try{
+//         const response = await axios.get(`https://api.github.com/users/${usuario}`);
+//         console.log(response.data);    
+//     }catch(error){
+//         console.log('Usuário não existe');
+//     }
+// }
+
+// buscaUsuario('Neylanio');
